cf3ac2d0bdc8eadb2b13fa9c00335aa08e54aeea
cvenegas-sernageomin <cvenegasbenavides@proton.me>
2026-06-19 12:03:28 -0400

    feat: AlertasKml con logica de color y alertas, tests

---
+++ src/AlertasKml.ps1
@@ -0,0 +1,98 @@
+function Get-ColorRedes([double]$mmH) {
+    if ($mmH -ge 10) { return 'rojo' }
+    if ($mmH -ge 5)  { return 'amarillo' }
+    return 'verde'
+}
+
+function Get-ColorEmas([double]$mmH, $iso) {
+    if ($null -eq $iso) { return 'verde' }
+    if ($mmH -ge 10 -and $iso -ge 3000) { return 'rojo' }
+    if ($mmH -ge 5  -and $iso -ge 3000) { return 'amarillo' }
+    return 'verde'
+}
+
+function Format-Epoch([long]$epoch) {
+    [DateTimeOffset]::FromUnixTimeSeconds($epoch).ToLocalTime().ToString('HH:mm dd-MMM-yyyy')
+}
+
+function Build-Styles {
+    return @"
+  <Style id="verde">
+    <IconStyle>
+      <color>ff00cc00</color><scale>0.6</scale>
+      <Icon><href>http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href></Icon>
+    </IconStyle>
+    <LabelStyle><scale>0</scale></LabelStyle>
+  </Style>
+  <Style id="amarillo">
+    <IconStyle>
+      <color>ff00ffff</color><scale>0.8</scale>
+      <Icon><href>http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href></Icon>
+    </IconStyle>
+    <LabelStyle><scale>0</scale></LabelStyle>
+  </Style>
+  <Style id="rojo">
+    <IconStyle>
+      <color>ff0000ff</color><scale>1.0</scale>
+      <Icon><href>http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png</href></Icon>
+    </IconStyle>
+    <LabelStyle><scale>0.7</scale></LabelStyle>
+  </Style>
+"@
+}
+
+function Build-PlacemarkRedes($e) {
+    $color = Get-ColorRedes $e.TasaMmH
+    $hora  = Format-Epoch $e.Epoch
+    $desc  = "<![CDATA[<b>$($e.Nombre)</b><br/>Red: $($e.Red)<br/>Precip: $($e.TasaMmH) mm/h<br/>Dato: $hora]]>"
+    return @"
+    <Placemark>
+      <name>$($e.Nombre) -- $($e.TasaMmH) mm/h</name>
+      <styleUrl>#$color</styleUrl>
+      <description>$desc</description>
+      <Point><coordinates>$($e.Lon),$($e.Lat),0</coordinates></Point>
+    </Placemark>
+"@
+}
+
+function Build-PlacemarkEmas($e) {
+    $color = Get-ColorEmas $e.TasaMmH $e.Isoterma
+    $hora  = Format-Epoch $e.Epoch
+    $isoStr  = if ($null -ne $e.Isoterma) { "$($e.Isoterma) m" } else { 'sin dato' }
+    $tempStr = if ($null -ne $e.TempC)    { "$($e.TempC) grados C"   } else { 'sin dato' }
+    $desc = "<![CDATA[<b>$($e.Nombre)</b><br/>Precip: $($e.TasaMmH) mm/h<br/>Temp: $tempStr<br/>Isoterma 0 grados C: $isoStr<br/>Altitud: $($e.Altitud) m<br/>Dato: $hora]]>"
+    return @"
+    <Placemark>
+      <name>$($e.Nombre) -- $($e.TasaMmH) mm/h</name>
+      <styleUrl>#$color</styleUrl>
+      <description>$desc</description>
+      <Point><coordinates>$($e.Lon),$($e.Lat),0</coordinates></Point>
+    </Placemark>
+"@
+}
+
+function Build-Kml([array]$redes, [array]$emas) {
+    $estilos = Build-Styles
+    $pmRedes = ($redes | ForEach-Object { Build-PlacemarkRedes $_ }) -join "`n"
+    $pmEmas  = ($emas  | ForEach-Object { Build-PlacemarkEmas $_ })  -join "`n"
+    $ts = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ssZ')
+    return @"
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+<Document>
+  <name>Alertas Redes Chile -- $ts</name>
+$estilos
+  <Folder>
+    <name>Redes CR2 -- Precipitacion ($($redes.Count) est.)</name>
+    <open>1</open>
+$pmRedes
+  </Folder>
+  <Folder>
+    <name>EMAs DMC -- Alerta completa ($($emas.Count) est.)</name>
+    <open>1</open>
+$pmEmas
+  </Folder>
+</Document>
+</kml>
+"@
+}
---
+++ tests/AlertasKml.Tests.ps1
@@ -0,0 +1,58 @@
+$here = $PSScriptRoot
+. "$here\..\src\AlertasKml.ps1"
+
+Describe "Get-ColorRedes" {
+    It "verde para 0 mm/h"  { Get-ColorRedes 0.0  | Should Be 'verde'    }
+    It "verde para 4.9"     { Get-ColorRedes 4.9  | Should Be 'verde'    }
+    It "amarillo para 5"    { Get-ColorRedes 5.0  | Should Be 'amarillo' }
+    It "amarillo para 9.9"  { Get-ColorRedes 9.9  | Should Be 'amarillo' }
+    It "rojo para 10"       { Get-ColorRedes 10.0 | Should Be 'rojo'     }
+    It "rojo para 50"       { Get-ColorRedes 50.0 | Should Be 'rojo'     }
+}
+
+Describe "Get-ColorEmas" {
+    # amarillo: precip>=5 Y iso>=3000
+    # rojo:     precip>=10 Y iso>=3000
+    It "verde cuando precip baja e iso baja" {
+        Get-ColorEmas 0.0 1000 | Should Be 'verde'
+    }
+    It "verde cuando precip alta pero iso baja" {
+        Get-ColorEmas 10.0 2999 | Should Be 'verde'
+    }
+    It "amarillo cuando precip>=5 Y iso>=3000" {
+        Get-ColorEmas 5.0 3000 | Should Be 'amarillo'
+    }
+    It "rojo cuando precip>=10 Y iso>=3000" {
+        Get-ColorEmas 10.0 3000 | Should Be 'rojo'
+    }
+    It "verde cuando iso es null" {
+        Get-ColorEmas 15.0 $null | Should Be 'verde'
+    }
+}
+
+Describe "Build-Kml" {
+    $redes = @(
+        [PSCustomObject]@{Nombre='Visviri'; Codigo='01K'; Lat=-17.5; Lon=-69.4; TasaMmH=0.0; Epoch=1781802000; Red='dga'}
+        [PSCustomObject]@{Nombre='Arica';   Codigo='02K'; Lat=-18.3; Lon=-70.3; TasaMmH=7.0; Epoch=1781802000; Red='dmc'}
+    )
+    $emas = @(
+        [PSCustomObject]@{Nombre='El Paico'; Codigo='330113'; Lat=-33.7; Lon=-71.0; Altitud=275.0; TasaMmH=6.5; TempC=8.0; Isoterma=1505; Epoch=1781807400}
+    )
+    $kml = Build-Kml $redes $emas
+
+    It "contiene declaracion XML" {
+        $kml | Should Match '<?xml'
+    }
+    It "contiene carpeta Redes CR2" {
+        $kml | Should Match 'Redes CR2'
+    }
+    It "contiene carpeta EMAs DMC" {
+        $kml | Should Match 'EMAs DMC'
+    }
+    It "contiene 3 placemarks en total" {
+        ($kml | Select-String '<Placemark>' -AllMatches).Matches.Count | Should Be 3
+    }
+    It "estacion redes con 7 mm/h aparece con styleUrl amarillo" {
+        $kml | Should Match '#amarillo'
+    }
+}
